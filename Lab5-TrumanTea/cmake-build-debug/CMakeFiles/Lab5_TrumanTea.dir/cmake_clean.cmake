7e92de6e284990c1a9f24ebada15f45572b8a289
Miles Lee <1789216340lhy@gmail.com>
2020-02-11 10:34:11 -0600

    Update Lab5

---
+++ Lab5-TrumanTea/cmake-build-debug/CMakeFiles/Lab5_TrumanTea.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab5_TrumanTea.dir/main.cpp.o"
+  "Lab5_TrumanTea"
+  "Lab5_TrumanTea.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab5_TrumanTea.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Lab5-TrumanTea/framework/Lab4TrumanTea/Lab4Template.cpp
@@ -0,0 +1,38 @@
+// @author: Your name
+// Purpose of the program
+
+#include<cstdlib>
+#include<ctime>
+#include<iomanip>
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  const double TWININGS_UNIT = 0.35;
+  const double TETLEY_UNIT = 0.85;
+  const double LIPTON_UNIT = 1.10;
+  
+  const int MAX_BONUS = 5;
+  const int MIN_BONUS = 1; 
+  
+  // generating a random number between (and including) MAX and MIN value
+  srand(time(0));
+  int bonus = 0;
+
+
+  //
+  cout << "Welcome to Truman Tea Store!" << endl;
+  cout << "Enter your name: ";
+  string name;
+  getline(cin, name);
+
+  cout << "How many ounces of Twinings, Tetley, and Lipton? ";
+  // declare variables and use cin to get values from the user
+  
+  // calcualte the price and printe them by using setw(), setprecision, fixed etc
+  
+  cout << "Thank you for ordering!" << endl;
+  
+  return 0;
+}
