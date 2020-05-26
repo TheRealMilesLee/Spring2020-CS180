0b4ab7e1d2813efacacc680da60441d6b3ebe0f9
Miles Lee <1789216340lhy@gmail.com>
2020-05-26 13:36:10 +0800

    更新

--- test/main.cpp
+++ test/main.cpp
@@ -1,31 +1,122 @@
-
 #include <iostream>
-#include <string>
-
 using namespace std;
 
-struct Movie
-{
-    string title;
-    double run_time;
-    int release_year;
-};
-
-string to_string(const Movie &movie);
+void eat(double meals);
+void sleep(unsigned hours);
+void code(unsigned time);
+void have_class(unsigned classes);
+void game(unsigned gametime);
 
 int main()
 {
-  int number = 6;
-  int x = 20;
-  x /= --number;
-  number *= x ++;
-  cout << x <<" " << number << endl;
+  double meals = 0;
+  unsigned hours =  24;
+  unsigned time = 0;
+  unsigned classes = 0;
+  unsigned gametime = 0;
+  bool death = false;
+
+  while(!death)
+  {
+    unsigned health_value = 1;
+    if (health_value > 0)
+    {
+      eat(meals);
+      sleep(hours);
+      code(time);
+      have_class(classes);
+      game(gametime);
+    }
+    else
+    {
+      death = true;
+    }
+  }
   return 0;
 }
-string to_string(const Movie &movie)
+
+void eat(double meals)
 {
-  string result = movie.title + "  " + to_string(movie.run_time) + "  " +
-                  to_string(movie.release_year);
-  return result;
+  bool hungry = false;
+  unsigned worktime = 0;
+  while (!hungry)
+  {
+    cout << "I'm not hungry, keep working" << endl;
+    worktime++;
+    if (worktime > 5)
+    {
+      hungry = true;
+    }
+    meals++;
+  }
 }
 
+void sleep(unsigned hours)
+{
+  for (unsigned time = 0; time < 24; time++)
+  {
+    if (time < 8 && time > 1)
+    {
+      hours = time + 6;
+    }
+    else
+    {
+      hours = 0;
+    }
+  }
+}
+
+void code(unsigned time)
+{
+  string day;
+  for (unsigned looptimes = 0; looptimes <= 7; looptimes++ )
+  {
+    cout << "What day is today? " << endl;
+    cin >> day;
+    if (day == "Tuesday" && day == "Thursday")
+    {
+      time == 12;
+    }
+    else
+    {
+      time == 2;
+    }
+  }
+}
+
+void have_class(unsigned classes)
+{
+  string day;
+  for (unsigned looptimes = 0; looptimes <= 7; looptimes++ )
+  {
+    cout << "What day is today? " << endl;
+    cin >> day;
+    if (day != "Saturday" && day != "Sunday")
+    {
+      classes = 2;
+    }
+    else
+    {
+      classes = 0;
+    }
+  }
+}
+
+void game(unsigned gametime)
+{
+  string day;
+  for (unsigned looptimes = 0; looptimes <= 7; looptimes++ )
+  {
+    cout << "What day is today? " << endl;
+    cin >> day;
+    bool paper_time;
+    paper_time = day != "Saturday" && day != "Sunday";
+    bool death = false;
+    unsigned happy_value = 0;
+    while (!paper_time && happy_value == 0)
+    {
+      gametime = 2;
+      happy_value++;
+    }
+  }
+}
